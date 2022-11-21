002936af0a3d13e89a2d0017f69629cc6896c5e5
Zeev <87901027+Zeev-x@users.noreply.github.com>
2022-11-21 20:14:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,31 +4,7 @@
     <title>CountDown</title>
   </head>
   <body>
-    <script>
-      var shiki = setInterval(function(){
-        var end = new Date("Feb 17,2023 00:00:00").getTime();
-        var now = new Date().getTime();
-        var dis = end - now;
-        
-        var iday = dis / (1000*60*60*24);
-        var ihour = (dis % (1000*60*60*24) / (1000*60*60));
-        var imin = (dis % (1000*60*60) / (1000*60));
-        var isec = (dis % (1000*60) / (1000));
-        
-        var day = Math.floor(iday);
-        var hour = Math.floor(ihour);
-        var min = Math.floor(imin);
-        var sec = Math.floor(isec);
-        
-        var res = day + " Days| " + hour + " Hours| " + min + " Minutes| " + sec + " Seconds ";
-        document.getElementById('count').innerHTML = res;
-        
-        if(dis < 0){
-          closeInterval(shiki);
-          document.getElementById('count').innerHTML = "Selamat Ulang Tahun";
-        }
-      },1000);
-    </script><br><br>
+    <script src="count.js"></script><br><br>
     <h1>Countdown</h1><br>
     <a id="count"></a>
     <br><br><br>
